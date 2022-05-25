81f4c67f9907d1b169ab7e7a8925e42a9f2081d2
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-05-25 22:48:23 +0600

    404 page completed

--- src/components/Pages/NotFound/NotFound.js
+++ src/components/Pages/NotFound/NotFound.js
@@ -1,9 +1,20 @@
 import React from "react";
+import notfound from "../../../image/notFound.gif";
 
 const NotFound = () => {
   return (
-    <div>
-      <h3>Im from NotFound</h3>
+    <div className="container mx-auto px-4 mt-10 lg:mt-20">
+      <div className="md:w-2/4 mx-auto">
+        <h2 className="text-3xl md:text-5xl text-center font-light z-20 relative">
+          Oops! page Not Found.
+        </h2>
+
+        <img
+          src={notfound}
+          alt="Not Found"
+          className="w-full mt-[-90px] md:mt-[-80px] z-[-10px] relative rounded-xl"
+        />
+      </div>
     </div>
   );
 };
